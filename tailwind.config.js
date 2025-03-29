97eb89609728aa425c96048bbf3fd7883684c483
Mursaleen00 <playessport@gmail.com>
2025-03-30 03:52:53 +0500

    `Feat`: - Set tailwind config - Add Images and Icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "text-gradient":
+          "linear-gradient(93deg, #8338EC 49.3%, #B923FF 87.76%)",
+      },
+      colors: {
+        background: "#0B001A",
+        primary: { DEFAULT: "#1E0044" },
+        white: "#E9DEFF",
+        black: "#0B001A",
+      },
+    },
   },
   plugins: [],
 };
