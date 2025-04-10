2b07e077ec43072336919e233af3561815a5bae4
SameerAli678 <alikhansameer234@gmail.com>
2025-04-11 02:13:09 +0500

    `fix` - hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 import ProjectCard from "@/components/cards/project-card";
 import Footer from "@/components/footer/footer";
+import HeroSection from "@/components/herosection/herosection";
 import { ProjectCardData } from "@/constant/project-card-data";
 
 export default function Home() {
   return (
     <div className="grid bg-background">
       <Footer />
+      <HeroSection />
       {ProjectCardData.map((item, i) => (
         <ProjectCard
           key={i}
